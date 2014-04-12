569a560f987d707a58290e04d1738a46c662953e
Andy <kooshaba@gmail.com>
2014-04-12 13:52:36 -0400

    feeds stuff

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var request = require('request');
 
-var weather = require('feeds/weather.js');
-var sky_feeds = require('feeds/sky_feeds.js');
-var maps = require('feeds/maps.js');
+var weather = require('./feeds/weather.js');
+var sky_feeds = require('./feeds/sky_feeds.js');
+var maps = require('./feeds/maps.js');
 
 var app = express();
 
@@ -14,11 +14,12 @@ app.get('/hello', function(req, res)
 	res.send('Hello World');
 });
 
-request('http://www.google.com', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body) // Print the google web page.
-  }
-});
+// Request example
+// request('http://www.google.com', function (error, response, body) {
+//   if (!error && response.statusCode == 200) {
+//     console.log(body) // Print the google web page.
+//   }
+// });
 
 app.listen(8000, function(){
 	console.log('Server started');
