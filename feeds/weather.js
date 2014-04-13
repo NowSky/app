4e0c81f70864f793c2486f4ea69abec4f14ef2b6
Andy <kooshaba@gmail.com>
2014-04-12 20:41:10 -0400

    implemented sunrise and sundown calls

--- feeds/weather.js
+++ feeds/weather.js
@@ -2,22 +2,47 @@ var request = require('request');
 require('../utils/read');
 var keys = require('../api_keys.json');
 
+function wunderground_url()
+{
+	return 'http://api.wunderground.com/api/' + keys.wunderground;
+}
+
+function get_night(lat, lon)
+{
+	request(wunderground_url() + '/astronomy/q/' + lat + ',' + lon + '.json', function(error, response, body)
+	{
+		if (!error && response.statusCode == 200)
+		{
+			var astronomy = JSON.parse(body);
+
+			// console.log(astronomy.sun_phase.sunrise.hour);
+			
+			return {
+				"sunrise_hour": astronomy.sun_phase.sunrise.hour,
+				"sunset_hour": astronomy.sun_phase.sunset.hour
+			}
+		}
+	});
+}
+
 function get_condition(lat, lon, hour, day)
 {
 	var hour_num = hour;
 	var day_num = day;
 
-	request('http://api.wunderground.com/api/' + keys.wunderground + '/hourly/q/' + lat + ',' + lon + '.json', function(error, response, body)
+	request(wunderground_url() + '/hourly/q/' + lat + ',' + lon + '.json', function(error, response, body)
 	{
 		if (!error && response.statusCode == 200)
 		{
 			var forecast = JSON.parse(body);
 
+			console.log(forecast['hourly_forecast']);
+
 			forecast['hourly_forecast'].forEach(function(time)
 			{
 				if(time.FCTTIME.hour == hour_num && time.FCTTIME.mday == day_num)
 				{
-					console.log(time.condition);
+					console.log(time);
 				}
 			});
 	  }
@@ -26,4 +51,5 @@ function get_condition(lat, lon, hour, day)
 }
 
 // Test
-get_condition('40.745695', '-74.02555389999999', 19, 12);
+// get_condition('40.745695', '-74.02555389999999', 19, 12);
+get_night('40.745695', '-74.02555389999999');
