5f0ca3625550047a5bff0bad6e8efe668312b5b3
Jeff Rothkirch <jeff.rothkirch@gmail.com>
2014-04-13 09:16:26 -0400

    changed format again

--- utils/getComets.js
+++ utils/getComets.js
@@ -24,7 +24,7 @@ fs.readFile('cometData.json', 'utf8', function (err,data) {
      var day = pdd(Number(event.JDate));
      var hour = pdh(Number(event.JDate));
      console.log(hour);
-     event.JDate = realDate;
+     event.JDate = day;
      event.Name0 = hour;
      // console.log(newDate);
      if (dateFrom < newDate && newDate < date2){
