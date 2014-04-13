d92c6fbb6517e9acdb0b38f33a91d9f6544e0cc8
Peter Goggi Jr <petergoggijr@gmail.com>
2014-04-13 04:20:24 -0400

    added route for the google places api

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var sky_feeds = require('./feeds/sky_feeds.js');
 var maps = require('./feeds/maps.js');
 var gmt = require('./feeds/gmt_time.js');
 var comets = require('./utils/getComets.js');
+var places = require('./feeds/places.js');
 
 var app = express();
 
@@ -14,6 +15,30 @@ app.use('/css', express.static(__dirname + '/public/css'));
 app.use('/scripts', express.static(__dirname + '/public/scripts'));
 app.use('/img', express.static(__dirname + '/public/img'));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
+
+app.get('/places/:query', function(req, res){
+    
+    var query = req.params.query;
+    
+    places.get_content(query, function(error, resp){
+	if(error)
+	{
+	    console.log(error);
+	}
+	else
+	{
+	    var result = resp;
+	    var resultArr = new Array();
+	    if(result['status'] == 'OK'){
+		for(i = 0; i < 5 && i < ['results'].length; i++){
+		    resultArr[i] = ['results'][i]['formatted_address'];
+		}
+	    }
+	    res.send(resultArr);
+	}
+    });
+}
+
 
 app.get('/events', function(req, res)
 {
