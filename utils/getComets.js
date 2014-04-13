27e450f1a31cd46386962b6ffeabae91b770b04f
Jeff Rothkirch <jeff.rothkirch@gmail.com>
2014-04-12 21:11:44 -0400

    gets comet json data for 30 days from todays date

---
+++ utils/getComets.js
@@ -0,0 +1,35 @@
+module.exports=function(){
+var date1 = new Date();
+var date2 = new Date();
+date2.setDate(date2.getDate() + 30);
+var x = 1;
+
+fs = require('fs')
+dc = require('./dateConvert.js')
+pd = require('./pullDate.js')
+
+fs.readFile('cometData.json', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+    var comets = JSON.parse(data);
+    var results = "";
+
+   comets.forEach(function(event){
+     var newDate = new Date(pd(Number(event.JDate)));
+     if (date1 < newDate && newDate < date2){
+        if (x > 1){
+            results = results + "," + JSON.stringify(event);
+        }
+        else {
+            results = results + JSON.stringify(event);
+        }
+      /*results = results + "," + JSON.stringify(event);*/
+     }
+   });
+   
+    /* console.log(results);*/
+    return results;
+ 
+});
+};
